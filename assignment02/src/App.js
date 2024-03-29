2e7f8d199308292992511e21d51072367785e08e
Kyle Jacobson <jacokyle01@gmail.com>
2024-03-29 13:22:23 -0500

    add searchbar with lowercase handling

--- assignment02/src/App.js
+++ assignment02/src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 const App = () => {
 	const [products, setProducts] = useState(productList);
+	const [query, setQuery] = useState("");
 
 	const addToCart = (which) => {
 		setProducts((prevProducts) => {
@@ -26,15 +27,16 @@ const App = () => {
 		});
 	};
 
-	const renderProducts = (products) => {
+	const renderProducts = (filteredProducts) => {
 		return (
 			<div id="products-wrap">
-				{products.map((product) => (
+				{filteredProducts.map((product) => (
 					<div className="visible-product">
+						<h1>{product.title}</h1>
 						<img alt="Product" src={product.images[0]} />
 						<div className="product-info">
-							<h3>{product.description}</h3>
-							<h2>{product.price}</h2>
+							<p>{product.description}</p>
+							<h2>${product.price}</h2>
 						</div>
 						<div className="cart-control">
 							<h2>{product.quantity}</h2>
@@ -56,14 +58,29 @@ const App = () => {
 			</div>
 		);
 	};
+
+	const updateQuery = (e) => {
+		setQuery(e.target.value);
+	};
+
+	const renderFilteredProducts = () => {
+        const filteredProducts = products.filter((product) => product.title.toLowerCase().includes(query.toLowerCase()))
+		return renderProducts(filteredProducts);
+	};
+
 	return (
 		<div id="main">
 			<header>
-				<input id="searchbar"></input>
-				<button id="search"></button>
-				<button id="checkout"></button>
+				<input
+					id="searchbar"
+					placeholder="Type to search"
+					value={query}
+					onChange={updateQuery}
+				></input>
+				<button id="search">Search</button>
+				<button id="checkout"> Checkout</button>
 			</header>
-			<main>{renderProducts(products)}</main>
+			<main>{renderFilteredProducts()}</main>
 		</div>
 	);
 };
