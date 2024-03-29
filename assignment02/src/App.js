5eccf6be1757cbeaefc29da18b240c4a8279e087
Kyle Jacobson <jacokyle01@gmail.com>
2024-03-29 11:33:00 -0500

    add product images

--- assignment02/src/App.js
+++ assignment02/src/App.js
@@ -1,6 +1,21 @@
 import { useState } from "react";
 import { products } from "./products";
 
+const renderProducts = (visibleProducts) => {
+	return (
+		<div>
+			{visibleProducts.map((product) => (
+				<div>
+					<img
+						alt="Product"
+						src={product.images[0]}
+						className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+					/>
+				</div>
+			))}
+		</div>
+	);
+};
 
 const App = () => {
 	const [visibleProducts, setVisibleProducts] = useState(products);
@@ -13,7 +28,9 @@ const App = () => {
 				<button id="checkout"></button>
 			</header>
 			<main>
-				<div id="products-wrap"></div>
+				<div id="products-wrap" className="ml-5 p-10 xl:basis-4/5">
+					{renderProducts(visibleProducts)}
+				</div>
 			</main>
 		</div>
 	);
